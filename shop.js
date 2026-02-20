7c21e2fc0bfda629703a2cac31043c9f35e770f7
swordahl <tylerlensebastianr@gmail.com>
2026-02-20 16:58:42 -0500

    Update shop.js

--- shop.js
+++ shop.js
@@ -10,9 +10,15 @@ const itemImage = document.getElementById("itemImage");
 const changeBtn = document.getElementById("changeItemBtn");
 
 changeBtn.addEventListener("click", () => {
+
+  // If only one item exists, do nothing
+  if (items.length <= 1) return;
+
   currentItem++;
+
   if (currentItem >= items.length) {
     currentItem = 0;
   }
+
   itemImage.src = items[currentItem];
 });
