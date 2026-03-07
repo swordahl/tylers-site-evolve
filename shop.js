edb839d83e0bc6f967d1a3a19cf5601c9587b5de
swordahl <tylerlensebastianr@gmail.com>
2026-03-07 03:29:57 -0500

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,21 +1,21 @@
 async function loadShop(){
 
-const res = await fetch("/content/shop/index.json");
-const data = await res.json();
+const response = await fetch("/content/shop/index.json");
+const data = await response.json();
 
 const wall = document.querySelector(".relic-wall");
 
 data.items.forEach(item => {
 
-const frame = document.createElement("section");
+const frame = document.createElement("div");
 frame.className = "relic-frame";
 
 frame.innerHTML = `
+<img src="/assets/shop-border1.png" class="border-art">
 
-<img src="/assets/shop-border/shop-border${item.border}.png" class="border-art">
-
+<div class="relic-safezone">
 <img src="${item.image}" class="relic-product">
-
+</div>
 `;
 
 wall.appendChild(frame);
